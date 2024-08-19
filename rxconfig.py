cf8c5ac479957ce5b47b29892c8fcac8d0fa1541
Jean bosco ninkam noumssi <78531307+jbcodefr@users.noreply.github.com>
2024-08-20 00:17:31 +0200

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="QuickMapp",
-    api_url="http://35.241.186.158:8000"
+    api_url="http://159.223.28.163:8000",
+    env=rx.Env.PROD
+    
 )
